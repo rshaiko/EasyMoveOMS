abc646ec8d2e3ade1de8054fdc2633c9ab260cb4
Roman Shaiko <34403190-rshaiko@users.noreply.github.com>
2018-03-27 01:24:24 -0400

    * Main window first design, some classes added

---
+++ EasyMoveOMS/EasyMoveOMS/Database.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMoveOMS
+{
+    class Database
+    {
+        public MySqlConnection conn;
+
+        public Database()
+        {
+            conn = new MySqlConnection("Server=den1.mysql5.gear.host; database=easymove; UID=easymove; password=Cz6iV4UR__0G");
+            conn.Open();
+        }
+    }
+
+}
