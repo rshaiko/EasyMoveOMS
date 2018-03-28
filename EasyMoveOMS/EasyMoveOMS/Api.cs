032fac25cb16ee8dc353f500dc8b0570ae249681
Roman Shaiko <34403190-rshaiko@users.noreply.github.com>
2018-03-28 13:04:44 -0400

    *resources added

---
+++ EasyMoveOMS/EasyMoveOMS/Api.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMoveOMS
+{
+    class Api
+    {
+        private string getJson(string url)
+        {
+            //url = @"https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=AIzaSyD0jS05VZtofCHL7mERZ9ibw91om5jwWX4";
+
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Stream data = response.GetResponseStream();
+            StreamReader reader = new StreamReader(data);
+            // json-formatted string from maps api
+            string responseFromServer = reader.ReadToEnd();
+            response.Close();
+            return responseFromServer;
+        }
+    }
+}
